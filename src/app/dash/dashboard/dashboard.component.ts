c911c158a103ce9c28fd17e330e4c630ce9347a4
Enriko Riba <eriba@span.eu>
2018-04-10 13:00:18 +0200

    .

--- src/app/dash/dashboard/dashboard.component.ts
+++ src/app/dash/dashboard/dashboard.component.ts
@@ -2,6 +2,7 @@ import { LoginService, Status } from './../../login.service';
 import { Component, OnInit } from '@angular/core';
 import { Observable } from 'rxjs/Observable';
 declare var googleyolo: any;
+declare var gapi: any;
 
 @Component({
   selector: 'app-dashboard',
@@ -33,6 +34,22 @@ export class DashboardComponent implements OnInit {
     }, (error)=> {
       if(error && error.type ==="userCanceled"){
         //this.status = Status.Canceled;
+      }else if(error && error.type ==="noCredentialsAvailable"){
+        gapi.load('auth2', function() {
+          gapi.auth2.authorize({
+            client_id: '679482392778-8gu33hgl4v7jaq8irc4ct9mi3u8o5g59.apps.googleusercontent.com',
+            scope: 'email profile openid',
+            response_type: 'id_token permission'
+          }, (response)=> {
+            if (response.error) {
+              // An error happened!
+              return;
+            }
+            // The user authorized the application for the scopes requested.
+            var accessToken = response.access_token;
+            var idToken = response.id_token;            
+          }); // gapi.auth2.authorize
+      }); //  gapi.load
       }
     });
   }
