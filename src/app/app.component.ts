ca1b7880cbaccb3bb2970c2200c4e849ee9e4dd1
Enriko Riba <eriba@span.eu>
2018-04-15 13:28:11 +0200

    .

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,7 +7,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  constructor(private client : GameClientService){}
+  constructor(public client : GameClientService){}
 
   title = 'My Hero';
 }
