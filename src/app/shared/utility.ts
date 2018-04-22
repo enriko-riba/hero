25a32f1e08930d33fca7aa671e6c2f225d82381a
Enriko Riba <eriba@span.eu>
2018-04-22 11:49:25 +0200

    .

--- src/app/shared/utility.ts
+++ src/app/shared/utility.ts
@@ -8,17 +8,27 @@ export type TimeStringUnit = 'ms' | 's';
  * @param unit unit of t: 'ms' for milliseconds, 's' for seconds
  */
 export function timeString(t: number, unit?: TimeStringUnit) {
-    let sec = unit == 's' ? t : t / 1000;
-    let h = Math.floor(sec / 3600);
-    sec -= h * 3600;
-    let min = Math.floor(sec / 60);
-    sec -= min * 60;
-    var result = "";
-    if (h)
-      result += `${h}h:`;
-    if (min)
-      result += `${min}m:`;
-
-    result += `${Math.floor(sec)}s`;
-    return result;
+  let sec = unit == 's' ? t : t / 1000;
+  let h = Math.floor(sec / 3600);
+  sec -= h * 3600;
+  let min = Math.floor(sec / 60);
+  sec -= min * 60;
+  var result = "";
+  if (h) {
+    result += `${h}h:`;
+    if (min || sec)
+      result += `${min}m`;
+    if (sec)
+      result += `:${Math.floor(sec)}s`;
+  } else {
+    if (min) {
+      result += `${min}m`;
+      if (sec)
+        result += `:${Math.floor(sec)}s`;
+    }
+    else if (sec)
+        result += `${Math.floor(sec)}s`;
   }
+
+  return result;
+}
